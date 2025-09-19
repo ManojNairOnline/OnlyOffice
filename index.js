071a73e06999de72425b98ea73936d9000f7132f
ManojNairOnline <manoj.nair@e.email>
2025-09-19 09:39:23 +0530

    Fix package loading issues - correct file references in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  OnlyOffice: require('./dist/nodes/OnlyOffice/OnlyOffice.node.js').OnlyOffice,
-  OnlyOfficeApi: require('./dist/credentials/OnlyOfficeApi.credentials.js').OnlyOfficeApi,
+  OnlyOffice: require('./dist/nodes/OnlyOffice/onlyoffice_node.js').OnlyOffice,
+  OnlyOfficeApi: require('./dist/credentials/onlyoffice_credentials.js').OnlyOfficeApi,
 };
